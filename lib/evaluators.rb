4854d54a20c1c6cbff14c6e2fdd4ee7dbc854dd9
Peter Lane <peter.c.r.lane@googlemail.com>
2013-01-02 09:41:51 +0000

    updated licence

--- lib/evaluators.rb
+++ lib/evaluators.rb
@@ -1,21 +1,9 @@
 # This file is part of svm_toolkit.
 #
 # Author::    Peter Lane
-# Copyright:: Copyright 2011, Peter Lane.
-# License::   GPLv3
+# Copyright:: Copyright 2011-13, Peter Lane.
+# License::   Open Works Licence
 #
-# svm_toolkit is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# svm_toolkit is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with svm_toolkit.  If not, see <http://www.gnu.org/licenses/>.
 
 # The Evaluator module provides some classes and methods to construct 
 # classes for evaluating the performance of a model against a dataset.  
